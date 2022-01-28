e5bf6ce882955977463b9cc513d0ebe0537ce1a6
H4ntek <85628860+H4ntek@users.noreply.github.com>
2022-01-28 08:06:37 +0100

    Update seg-tree-lite.cpp

--- seg-tree-lite.cpp
+++ seg-tree-lite.cpp
@@ -1,34 +1,33 @@
 /*
 CHANGE op AND neutral BEFORE USING
-OPTIONAL beggining array a (if there is one uncomment "build" function in init)
+OPTIONAL beggining array a (if there is one uncomment "build" function)
 Seg_tree st;
 st.init(n) - initializes a segment tree for array of size n
-st.query(1, 0, n - 1, l, r) - returns query on [l, r]
-st.update(1, 0, n - 1, i, v) - updates (adds or sets) a[i] to v
+st.query(l, r) - returns query on [l, r]
+st.update(i, v) - updates (adds or sets) v at index i
+st.print() - print for debugging
 */
 struct Seg_tree{
     const int INF_INT = 1e9 + 3;
     const lli INF_LL = 1e18 + 3;
     int base;
-    vector <lli> st;
+    vector <int> st;
 
-    lli op(lli a, lli b){ //max, min, sum, xor, gcd...
-        return a + b;
+    int op(int a, int b){ //max, min, sum, xor, gcd...
+        return __gcd(a, b);
     }
 
-    lli neutral(){ //0 for sum, xor, gcd, -INF for max, INF for min
+    int neutral(){ //0 for sum, xor, gcd, -INF for max, INF for min
         return 0;
     }
 
-    void build(int si, int ss, int se){
-        if (ss == se){
-            //st[si] = a[ss];
-            return;
+    void build(int n){
+        for (int i = 0; i < n; i++){
+            //st[base + i] = a[i];
         }
-        int mid = (ss + se) / 2;
-        build(2 * si, ss, mid);
-        build(2 * si + 1, mid + 1, se);
-        st[si] = op(st[2 * si], st[2 * si + 1]);
+        for (int si = base - 1; si >= 1; si--){
+            st[si] = op(st[2 * si], st[2 * si + 1]);
+        }
     }
 
     void init(int n){
@@ -37,33 +36,44 @@ struct Seg_tree{
             base *= 2;
         }
         st.assign(2 * base, neutral());
-        //build(1, 0, n - 1);
+        //build(n);
     }
 
-    lli query(int si, int ss, int se, int qs, int qe){ 
-        if (ss > qe || se < qs){
-            return neutral();
+    int query(int qs, int qe){
+        int si_l = base + qs, si_r = base + qe, res = st[si_l];
+        if (si_l != si_r){
+            res = op(res, st[si_r]);
         }
-        if (ss >= qs && se <= qe){
-            return st[si];
+        while (si_l / 2 != si_r / 2){
+            if (si_l % 2 == 0){
+                res = op(res, st[si_l + 1]);
+            }
+            si_l /= 2;
+            if (si_r % 2 == 1){
+                res = op(res, st[si_r - 1]);
+            }
+            si_r /= 2;
         }
-
-        int mid = (ss + se) / 2;
-        return op(query(2 * si, ss, mid, qs, qe), query(2 * si + 1, mid + 1, se, qs, qe));
+        return res;
     }
 
-    void update(int si, int ss, int se, int ui, lli val){ 
-        if (ss == se){
-            st[si] = val;
-            return;
+    void update(int ui, int val){
+        int si = base + ui;
+        st[si] = val;
+        while (si > 1){
+            si /= 2;
+            st[si] = op(st[2 * si], st[2 * si + 1]);
         }
-        int mid = (ss + se) / 2;
-        if (ui <= mid){
-            update(2 * si, ss, mid, ui, val);
+    }
+
+    void print(){
+        int p = 1;
+        while (p <= base){
+            for (int i = 0; i < p; i++){
+                cout << st[i + p] << " ";
+            }
+            cout << "\n";
+            p *= 2;
         }
-        else{
-            update(2 * si + 1, mid + 1, se, ui, val);
-        }
-        st[si] = op(st[2 * si], st[2 * si + 1]);
     }
 };
