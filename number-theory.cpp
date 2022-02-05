03f03857aa7e63da4ba51905dbc9710fe0bbf220
H4ntek <85628860+H4ntek@users.noreply.github.com>
2022-02-05 22:08:51 +0100

    Update number-theory.cpp

--- number-theory.cpp
+++ number-theory.cpp
@@ -1,81 +1,78 @@
-namespace math{
-    const int SIEVE_SZ = 10e6 + 3;
-    bool prime[SIEVE_SZ];
+const int SIEVE_SZ = 10e6 + 3;
+bool prime[SIEVE_SZ];
 
-    bool is_prime(lli n){
-        if (n <= 1) return false;
-        if (n <= 3) return true;
-        if (n % 2 == 0 || n % 3 == 0) return false;
-        
-        for (int i = 5; (lli) i * i <= n; i += 6){
-            if (n % i == 0 || n % (i + 2) == 0){
-                return false;
-            }
-        }
-        return true;
-    }
-    
-    void sieve(int n){
-        prime[0] = prime[1] = false;
-        for (int i = 2; i <= n; i++){
-            prime[i] = true;
-        }
-        for (int i = 2; i * i <= n; i++){
-            if (prime[i]){
-                for (int j = i * i; j <= n; j += i){
-                    prime[j] = false;
-                }
-            }
+bool is_prime(lli n){
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+
+    for (int i = 5; (lli) i * i <= n; i += 6){
+        if (n % i == 0 || n % (i + 2) == 0){
+            return false;
         }
     }
+    return true;
+}
 
-    bool generated = false;
-    vector <lli> primes;
-    void gen_primes(int n){
-        generated = true;
-        sieve(n);
-        for (int i = 2; i <= n; i++){
-            if (prime[i]){
-                primes.push_back((lli) i);
-            }
-        }
+void sieve(int n){
+    prime[0] = prime[1] = false;
+    for (int i = 2; i <= n; i++){
+        prime[i] = true;
     }
-
-    vector <pair <lli, int>> factorize(lli n){ 
-        vector <pair <lli, int>> v;
-        if (!generated){
-            cout << "Primes not generated.\n";
-            return v;
-        }
-        int cnt;
-        for (int i = 0; primes[i] * primes[i] <= n; i++){
-            cnt = 0;
-            while (n % primes[i] == 0){
-                cnt++;
-                n /= primes[i];
-            }
-            if (cnt){
-                v.push_back({primes[i], cnt});
+    for (int i = 2; i * i <= n; i++){
+        if (prime[i]){
+            for (int j = i * i; j <= n; j += i){
+                prime[j] = false;
             }
         }
-        if (n > 1){
-            v.push_back({n, 1});
-        }
-        return v;
-    }
-
-    vector <lli> get_divs(lli n){ //unsorted
-        vector <lli> v;
-        for (lli i = 1; i * i <= n; i++){
-            if (i * i == n){
-                v.push_back(i);
-            }
-            else if (n % i == 0){
-                v.push_back(i);
-                v.push_back(n / i);
-            }
-        }
-        return v;
     }
 }
-using namespace math;
+
+bool generated = false;
+vector <lli> primes;
+void gen_primes(int n){
+    generated = true;
+    sieve(n);
+    for (int i = 2; i <= n; i++){
+        if (prime[i]){
+            primes.push_back((lli) i);
+        }
+    }
+}
+
+vector <pair <lli, int>> factorize(lli n){ 
+    vector <pair <lli, int>> v;
+    if (!generated){
+        cout << "Primes not generated.\n";
+        return v;
+    }
+    int cnt;
+    for (int i = 0; primes[i] * primes[i] <= n; i++){
+        cnt = 0;
+        while (n % primes[i] == 0){
+            cnt++;
+            n /= primes[i];
+        }
+        if (cnt){
+            v.push_back({primes[i], cnt});
+        }
+    }
+    if (n > 1){
+        v.push_back({n, 1});
+    }
+    return v;
+}
+
+vector <lli> get_divs(lli n){ //unsorted
+    vector <lli> v;
+    for (lli i = 1; i * i <= n; i++){
+        if (i * i == n){
+            v.push_back(i);
+        }
+        else if (n % i == 0){
+            v.push_back(i);
+            v.push_back(n / i);
+        }
+    }
+    return v;
+}
