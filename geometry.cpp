fe06b73daa90e7551f3b32ffbd22db5cf028989a
H4ntek <85628860+H4ntek@users.noreply.github.com>
2022-02-06 08:45:06 +0100

    Update geometry.cpp

--- geometry.cpp
+++ geometry.cpp
@@ -25,6 +25,30 @@ struct P{
     }
 };
 
+int sgn(lli x){
+    if (x == 0) return 0;
+    return (x > 0 ? 1 : -1);
+}
+
+bool proj(lli a, lli b, lli c, lli d){
+    if (a > b) swap(a, b);
+    if (c > d) swap(c, d);
+    return max(a, c) <= min(b, d);
+}
+
+bool intersect(P A, P B, P C, P D){
+    int o1 = sgn(A.orient(B, C)), o2 = sgn(A.orient(B, D)), o3 = sgn(C.orient(D, A)), o4 = sgn(C.orient(D, B));
+    if (o1 != o2 && o3 != o4){
+        return true;
+    }
+    if (o1 == 0 && o4 == 0){
+        if (proj(A.x, B.x, C.x, D.x) && proj(A.y, B.y, C.y, D.y)){
+            return true;
+        }
+    }
+    return false;
+}
+
 lli area(vector <P> &poly){ //actually 2 * area
     lli res = 0;
     int n = poly.size();
@@ -32,4 +56,29 @@ lli area(vector <P> &poly){ //actually 2 * area
         res += poly[i] * poly[j];
     }
     return abs(res);
+}
+
+vector <P> convex_hull(vector <P> points){
+    if ((int) points.size() <= 3){
+        return points;
+    }
+    vector <P> ch;
+    sort(points.begin(), points.end());
+    for (P p : points){
+        while ((int) ch.size() >= 2 && p.orient(ch[ch.size() - 2], ch[ch.size() - 1]) > 0){
+            ch.pop_back();
+        }
+        ch.emplace_back(p);
+    }
+    ch.pop_back();
+    int sz = ch.size();
+    reverse(points.begin(), points.end());
+    for (P p : points){
+        while ((int) ch.size() >= sz + 2 && p.orient(ch[ch.size() - 2], ch[ch.size() - 1]) > 0){
+            ch.pop_back();
+        }
+        ch.emplace_back(p);
+    }
+    ch.pop_back();
+    return ch;
 }
