e7fba6425606c29ae0e4f48269f59b3acd15fb01
H4ntek <85628860+H4ntek@users.noreply.github.com>
2022-02-09 17:00:38 +0100

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -1,35 +1,35 @@
-const int SZ = 7, MOD = 1e9 + 7;
+const int SZ = 2, MOD = 1e9 + 7;
 
-/*
-Mat m;
-m2 = m.pow(m, k) - m2 becomes m^k
-m.m[i][j] - element in i-th row, j-th column
-*/
-struct Mat{
-    vector<vector<lli>> m = vector<vector<lli>>(SZ, vector <lli>(SZ));
-    Mat operator *(Mat& m2){
-        Mat prod;
-        for (int i = 0; i < SZ; i++){
-            for (int j = 0; j < SZ; j++){
-                for (int k = 0; k < SZ; k++){
-                    prod.m[i][k] = (prod.m[i][k] + m[i][j] * m2.m[j][k]) % MOD;
-                }
-            }
-        }
-        return prod;
+using Mat = vector<vector<lli>>;
+
+Mat mul(Mat A, Mat B) {
+	Mat res(SZ);
+    for (int i = 0; i < SZ; i++){
+        res[i].resize(SZ, 0);
     }
-    Mat pow(Mat a, lli b){
-        Mat prod;
-        for (int i = 0; i < SZ; i++){
-            prod.m[i][i] = 1;
-        }
-        while (b){
-            if (b % 2 == 1){
-                prod = prod * a;
-            }
-            a = a * a;
-            b /= 2;
-        }
-        return prod;
+	for (int i = 0; i < SZ; i++) {
+		for (int j = 0; j < SZ; j++) {
+			for (int k = 0; k < SZ; k++) {
+				res[i][j] += A[i][k] * B[k][j];
+				res[i][j] %= MOD;
+			}
+		}
+	}
+	return res;
+}
+
+Mat power(Mat A, lli b){
+    Mat I(SZ);
+    for (int i = 0; i < SZ; i++){
+        I[i].resize(SZ, 0);
+        I[i][i] = 1;
     }
-};
+    while (b){
+        if (b % 2 == 1){
+            I = mul(I, A);
+        }
+        A = mul(A, A);
+        b /= 2;
+    }
+    return I;
+}
