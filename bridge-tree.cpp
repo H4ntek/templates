a22684c9663c34e20788c25bc26c9495aabcb62d
H4ntek <85628860+H4ntek@users.noreply.github.com>
2022-02-13 13:24:53 +0100

    Update bridge-tree.cpp

--- bridge-tree.cpp
+++ bridge-tree.cpp
@@ -1,6 +1,7 @@
 /*
 Bridge tree - condenses graph into a tree, where vertices are 2-edge-connected components and edges <==> bridges from the original graph.
-REQUIRES adj (graph) with pairs {u, edge_id}, list of edges, is_bridge, in, low, comp
+REQUIRES adj (graph) with pairs {u, edge_id}, list of edges, is_bridge, in, low, comp, comp_no, timer
+MEMSET in and comp to -1
 */
 void DFS_bridges(int v, int par){
     in[v] = low[v] = timer++;
